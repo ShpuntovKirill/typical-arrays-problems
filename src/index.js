4634c65521a3663ba8af0acc606e26e5a4620977
Shpuntov Kirill <ks090192@gmail.com>
2020-09-29 23:14:28 +0300

    init: done typical arrays problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    var minValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+      const currentValue = array[i];
+      if (currentValue < minValue) {
+        minValue = currentValue;
+      }
+    } return minValue;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    var maxValue = array[0];
+    for (let i = 0; i < array.length; i++) {
+      const currentValue = array[i];
+      if (currentValue > maxValue) {
+        maxValue = currentValue;
+      }
+    } return maxValue;
+  } 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  } else {
+    var sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    } return sum / array.length;
+  } 
 }
